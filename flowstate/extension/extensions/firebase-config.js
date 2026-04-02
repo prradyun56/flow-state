21e98283767faf58ffadabab01b7bb20bcbbf2cf
shikhar102005 <shikhar.pandey2025@vitstudent.ac.in>
2026-04-02 19:32:16 +0530

    final implementation

---
+++ flowstate/extension/extensions/firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZ13a8V6j88Jh4cZo8R0Dn75LWvGNGWaI",
+  authDomain: "flowstate-be563.firebaseapp.com",
+  projectId: "flowstate-be563",
+  storageBucket: "flowstate-be563.firebasestorage.app",
+  messagingSenderId: "488894823727",
+  appId: "1:488894823727:web:e6fb775e9a41165e8db5da",
+  measurementId: "G-CGFRW55CV5"
+};
+
+// Google OAuth Client ID for Chrome Identity API
+// Get this from Google Cloud Console > APIs & Services > Credentials > OAuth 2.0 Client ID (Web application)
+// Set redirect URI to: https://<your-extension-id>.chromiumapp.org/
+export const GOOGLE_CLIENT_ID = "488894823727-k3nrmcpcun4u60d0n7144lf4p2dfoec2.apps.googleusercontent.com";
